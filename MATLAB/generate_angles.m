c11d056827b37da0bfcf59cf4c179904333172ea
riccardodeleoni10 <riccardo.deleoni10@gmail.com>
2025-11-20 23:13:46 +0100

    Create generate_angles.m

---
+++ MATLAB/generate_angles.m
@@ -0,0 +1,14 @@
+%% Script to generate binary values of the angles
+N_tot = 16;
+N_frac = 14;
+fid = fopen('angles.txt','w');
+%% Circular coordinates angles
+for i=0:N_tot-1
+    temp = atan(2^-i);
+    temp2 = temp * 2^(N_frac);
+    binaryValue = round(temp2); % Convert to binary representation
+    temp3 = dec2base(binaryValue,2,16);
+    fprintf(fid,'%s\n',temp3);
+    fprintf('Angle %d: Binary Value = %d\n', i, binaryValue); % Display the result  
+end
+fclose(fid);
